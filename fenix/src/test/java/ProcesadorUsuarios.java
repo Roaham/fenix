bc43a7ebfae2417c54b95aca19ccfb9e1f258768
Roaham <roripro2007@gmail.com>
2026-01-19 10:35:28 +0100

    Documentación Javadoc añadida

--- fenix/src/test/java/ProcesadorUsuarios.java
+++ fenix/src/test/java/ProcesadorUsuarios.java
@@ -1,11 +1,27 @@
 import java.util.List;
 
 /**
- * Esta clase procesa listas de usuarios. Contiene 'code smells' intencionados.
+ * Procesa y clasifica una lista de usuarios según sus roles.
+ * Separa a los usuarios en categorías de Administradores e Invitados basándose en un formato de cadena de texto específico.
+ * * @author Roaham
+ * @version 1.0
  */
+
 public class ProcesadorUsuarios {
 
-    // Método con 'code smells': largo, números mágicos, malos nombres.
+/**
+ * Procesa una lista de cadenas (listas, parecidas a las de python) con formato "nombre:rol" y devuelve un resumen.
+ * <p>
+ * Los roles permitidos son:
+ * <ul>
+ * <li>1: Administradores</li>
+ * <li>2: Invitados</li>
+ * </ul>
+ *
+ * @param usuarios Lista de strings donde cada elemento sigue el formato "nombre:ID_ROL".
+ * @return Una cadena con los nombres de los administradores e invitados.
+ */
+
     public String procesarLista(List<String> dataList) {
         String admins = "";
         String invitados = "";
@@ -29,10 +45,20 @@ public class ProcesadorUsuarios {
         return "Admins: " + admins + " | Invitados: " + invitados;
     }
 
+/**
+ * Añade un nombre a la cadena acumulada de administradores.
+ * @param nombre Nombre del administrador a añadir.
+ */
+
     private String procesarAdmin(String admins, String n) {
         admins += n + ",";
         return admins;
     }
+
+/**
+ * Añade un nombre a la cadena acumulada de invitados.
+ * @param nombre Nombre del invitado a añadir.
+ */
 
     private String procesarInvitado(String invitados, String n) {
         invitados += n + ",";
